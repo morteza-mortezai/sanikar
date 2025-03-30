927832769534558f8b4bd85ab75a39d60bf124a8
morteza-mortezai <mrtz.mrtzi.1989@gmail.com-at-003669155861>
2025-03-31 00:19:17 +0330

    fix bug in migration

--- database/migrations/2025_03_30_194650_create_task_table.php
+++ database/migrations/2025_03_30_194650_create_task_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('task', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('task');
+        Schema::dropIfExists('tasks');
     }
 };
